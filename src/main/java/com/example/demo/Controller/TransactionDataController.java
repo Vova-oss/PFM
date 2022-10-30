139fa5763c63eb35607e68aaba08b1317acdb402
Vova-oss <56549937+Vova-oss@users.noreply.github.com>
2022-10-30 22:01:53 +0300

    Update TransactionDataController.java

--- src/main/java/com/example/demo/Controller/TransactionDataController.java
+++ src/main/java/com/example/demo/Controller/TransactionDataController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Api(tags = "TransactionData")
 @RestController
 @CrossOrigin("https://hack.saizaax.xyz")
-@RequestMapping("/transactions")
+@RequestMapping("/api/transactions")
 public class TransactionDataController {
 
     @Autowired
